d0d0b157f2bd63c61bd0414ada9192781aaf44a6
Oluwajuwon Eletu <126976305+Juwonlor@users.noreply.github.com>
2024-10-08 11:42:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+alert("Simple project built by juwonlor")
+document.write("<br>");
+document.write("<br>");
+
+// // calculation for multiplication//
+ const gravity = 10;
+let mass = 15;
+ let force = mass * gravity;
+ document.write("The value of Force is =" + force);
+document.write("<br>");
+
+// // division of numbers//
+ let month = 5;
+  const Age = 20;
+ let  average = Age/ month;
+  document.write("The Averge number is =" + average);
+ document.write("<br>");
+
+// // subtraction//
+ var lagosIsland = 48;
+ var lekki = 50;
+ var mushin= 25;
+ var total = 150;
+var ojo = total-lagosIsland - lekki - mushin;
+document.write("The Total Number of People in Ojo is =" + ojo);
+document.write("<br>");
+
+
+// //  addition//
+const age = 15;
+ var boy = 12;
+ mother = age + boy;
+ father = mother + boy + 4;
+ document.write("The value for the Father is" + father);
+ document.write("<br>");
+
+//  the power of a number//
+const preg1 = 2
+var year2 = 4
+var year5 = 5**2
+document.write("The Number of babies is" + year5)
+document.write("<br>");
+
+
+// percentage of money//
+const SAlary= 100000;
+let pay = 10;
+tithe = pay % SAlary;
+document.write("My tithe is" + tithe);
+
+
+
+
+
